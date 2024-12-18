552c4663d5d78db132609e10f94107f3a821a6e6
LeNelSoN <nelis.valentin@gmail.com>
2024-12-19 00:29:47 +0100

    Initial commit

---
+++ src/main/java/com/edj/teamoop/TeamoopApplication.java
@@ -0,0 +1,13 @@
+package com.edj.teamoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamoopApplication.class, args);
+	}
+
+}
