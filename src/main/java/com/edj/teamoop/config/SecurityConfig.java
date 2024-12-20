12ea85958b2e068bfd55dd57b137d3e2b6975b10
LeNelSoN <nelis.valentin@gmail.com>
2024-12-20 12:20:09 +0100

    feat(config): add cors-config

--- src/main/java/com/edj/teamoop/config/SecurityConfig.java
+++ src/main/java/com/edj/teamoop/config/SecurityConfig.java
@@ -1,16 +1,22 @@
 package com.edj.teamoop.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    CorsConfig corsConfig;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 
--- src/main/java/com/edj/teamoop/controller/HelloController.java
+++ src/main/java/com/edj/teamoop/controller/HelloController.java
@@ -1,11 +1,15 @@
 package com.edj.teamoop.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http:localhost:4200")
 public class HelloController {
-    @GetMapping()
+    @GetMapping("/hello")
     public String Hello() {
         return "Hello, les devs!";
     }
