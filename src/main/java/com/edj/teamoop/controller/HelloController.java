ad75700c85c87252c52d75ffa0f5989df7e901e1
LeNelSoN <nelis.valentin@gmail.com>
2024-12-20 12:38:25 +0100

    remove annotation cors on HelloController

--- src/main/java/com/edj/teamoop/controller/HelloController.java
+++ src/main/java/com/edj/teamoop/controller/HelloController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http:localhost:4200")
 public class HelloController {
     @GetMapping("/hello")
     public String Hello() {
