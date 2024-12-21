bd6cd7c9443298f4d495dac568184b7b2cf79735
Allan Serre <allan.serre@etu.univ-lehavre.fr>
2024-12-21 20:50:01 +0100

    Add WebMvcTest and Database communication

--- src/main/java/com/edj/teamoop/TeamoopApplication.java
+++ src/main/java/com/edj/teamoop/TeamoopApplication.java
@@ -9,5 +9,4 @@ public class TeamoopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TeamoopApplication.class, args);
 	}
-
 }
---
+++ src/test/java/com/edj/teamoop/controller/HelloControllerTest.java
@@ -0,0 +1,26 @@
+package com.edj.teamoop.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@WebMvcTest(HelloController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void helloControllerShouldReturnMessage() throws Exception {
+        mockMvc.perform(get("/api/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello, les devs!"));
+    }
+}
