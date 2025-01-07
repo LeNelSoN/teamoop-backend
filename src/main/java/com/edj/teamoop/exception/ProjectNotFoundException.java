3ec93b1137db4b2785b6b50fdda7ece26f67f806
LeNelSoN <nelis.valentin@gmail.com>
2025-01-07 15:48:25 +0100

    change noarg constructor

--- src/main/java/com/edj/teamoop/exception/ProjectNotFoundException.java
+++ src/main/java/com/edj/teamoop/exception/ProjectNotFoundException.java
@@ -4,7 +4,7 @@ public class ProjectNotFoundException extends RuntimeException {
     public ProjectNotFoundException(String message) {
         super(message);
     }
-    public ProjectNotFoundException(Long id) {
-        super(String.format("The project with ID %s does not exist.", id));
+    public ProjectNotFoundException() {
+        super("The project does not exist.");
     }
 }
