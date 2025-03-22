61ff8f86e6be245277f132793d468a68a9ebf890
GuillaumeSUBRA1 <74560838+GuillaumeSUBRA1@users.noreply.github.com>
2025-03-22 19:03:45 +0100

    remove dead import

--- src/main/java/com/edj/teamoop/controller/ProjectController.java
+++ src/main/java/com/edj/teamoop/controller/ProjectController.java
@@ -4,7 +4,6 @@ import com.edj.teamoop.dto.ProjectDTO;
 import com.edj.teamoop.service.cache.ProjectCacheService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
