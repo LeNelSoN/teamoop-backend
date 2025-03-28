b9d987093bda83bca09aa2fe6ef69a27e27555b7
LeNelSoN <nelis.valentin@gmail.com>
2025-03-28 13:07:03 +0100

    remove useless import

--- src/main/java/com/edj/teamoop/utility/SecurityContextUtil.java
+++ src/main/java/com/edj/teamoop/utility/SecurityContextUtil.java
@@ -1,6 +1,5 @@
 package com.edj.teamoop.utility;
 
-import com.edj.teamoop.model.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
